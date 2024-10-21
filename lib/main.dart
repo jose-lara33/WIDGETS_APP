bc4b85f796d56211cd26fbb15a2414a47fce03f6
jose muñoz lara <josee.lara@gmail.com>
2024-10-21 09:26:15 +0200

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:widgets_app/config/router/app_router.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'FLUTTER WIDGETS',
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 0, selctedcolor: 0).getTheme(),
+    );
+  }
+}
